0f4d3acdf461d3c8323d88510ca95cadc8fd561e
hassan-ali-OTS <hassan.ali@onetechnologyservices.com>
2023-06-20 17:46:27 +0500

    controller changed

--- Basics/src/messages/messages.controller.ts
+++ Basics/src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message-dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -6,7 +7,7 @@ export class MessagesController {
   listMessages() {}
 
   @Post()
-  createMessage(@Body() body: any) {
+  createMessage(@Body() body: CreateMessageDto) {
     console.log(body);
   }
 
