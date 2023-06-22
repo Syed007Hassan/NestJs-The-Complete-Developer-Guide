e990beab9df4c8099e98df814ac120a6f75c78c0
hassan-ali-OTS <hassan.ali@onetechnologyservices.com>
2023-06-22 11:42:56 +0500

    unit testing

--- Basics/test/app.e2e-spec.ts
+++ Basics/test/app.e2e-spec.ts
@@ -1,24 +1,74 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { MessagesModule } from '../src/messages/messages.module';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('MessagesController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [MessagesModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('/messages (GET)', () => {
+    it('should return an array of messages', () => {
+      return request(app.getHttpServer())
+        .get('/messages')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toBeInstanceOf(Object);
+        });
+    });
+  });
+
+  describe('/messages (POST)', () => {
+    it('should create a new message', () => {
+      const message = { content: 'Hello, world!' };
+
+      return request(app.getHttpServer())
+        .post('/messages')
+        .send(message)
+        .expect(201)
+        .expect((res) => {
+          expect(res.body).toHaveProperty('id');
+          expect(res.body.content).toEqual(message.content);
+        });
+    });
+  });
+
+  describe('/messages/:id (GET)', () => {
+    it('should return a message by ID', async () => {
+      const message = { content: 'Hello, world!' };
+      const createdMessage = await request(app.getHttpServer())
+        .post('/messages')
+        .send(message)
+        .expect(201);
+
+      return request(app.getHttpServer())
+        .get(`/messages/${createdMessage.body.id}`)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toHaveProperty('id');
+          expect(res.body.content).toEqual(message.content);
+        });
+    });
+
+    it('should return a 404 error if message is not found', () => {
+      return request(app.getHttpServer())
+        .get('/messages/999')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body.message).toEqual('message not found');
+        });
+    });
   });
 });
