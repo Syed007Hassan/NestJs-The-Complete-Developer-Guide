3d1a332fb58982ebb59e27e77f058a21c20d2197
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-06-24 14:56:31 +0500

    service added

--- di/src/power/power.service.ts
+++ di/src/power/power.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PowerService {}
+export class PowerService {
+  supplyPower(wats: number) {
+    return `Supplying ${wats} watts of power.`;
+  }
+}
