7c938f21ab2237bd601ebf59baf83af0e148ddc1
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-06-26 01:48:53 +0500

    TypeORM

--- mycarvalue/src/app.module.ts
+++ mycarvalue/src/app.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
+
 @Module({
-  imports: [],
+  imports: [ReportsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
