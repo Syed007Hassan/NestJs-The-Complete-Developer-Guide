4d7e0288b0d8984de8b1e2e14943db7d7e506cd9
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-06-29 18:59:39 +0500

    user service added

--- mycarvalue/src/users/users.controller.ts
+++ mycarvalue/src/users/users.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/create-user-dto';
 
 @Controller('auth')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('/signup')
-  async createUser() {}
+  async createUser(@Body() body: CreateUserDto) {
+    this.usersService.create(body.email, body.password);
+  }
 }
--- mycarvalue/src/users/users.service.ts
+++ mycarvalue/src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './users.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  create(email: string, password: string) {
+    const user = this.usersRepository.create({ email, password });
+    return this.usersRepository.save(user);
+  }
+}
