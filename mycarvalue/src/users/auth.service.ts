343e366063b31d3c746031558ef0533d2db2cf13
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-07-06 21:49:55 +0500

    signUp service added

--- mycarvalue/src/users/auth.service.ts
+++ mycarvalue/src/users/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common/decorators';
 import { UsersService } from './users.service';
 import { BadRequestException } from '@nestjs/common';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
 
+const scrypt = promisify(_scrypt);
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
@@ -11,8 +14,16 @@ export class AuthService {
 
     const users = await this.usersService.findByEmail(email);
 
-    if (!users) {
+    if (users) {
       throw new BadRequestException('Email already in use');
     }
+
+    const salt = randomBytes(8).toString('hex'); //generate salt
+    const hash = (await scrypt(password, salt, 32)) as Buffer; //hash users password
+    const result = salt + '.' + hash.toString('hex'); //store salt and hash together
+
+    const user = await this.usersService.create(email, result); //create user
+
+    return user;
   }
 }
--- mycarvalue/src/users/users.controller.ts
+++ mycarvalue/src/users/users.controller.ts
@@ -14,15 +14,19 @@ import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 import { UserDto } from './dtos/user.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
+    return this.authService.signup(body.email, body.password);
   }
 
   // @UseInterceptors(new SerializeInterceptor(UserDto))
