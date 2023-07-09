115c957e8d0effa91bf676c9696cba7a030f354d
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-07-09 18:08:57 +0500

    decorator changed

--- mycarvalue/src/users/decorators/current-user-decorator.ts
+++ mycarvalue/src/users/decorators/current-user-decorator.ts
@@ -1,8 +1,10 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
-  (data: any, context: ExecutionContext) => {
-    const request = context.switchToHttp().getRequest();
-    return request.currentUser;
+  (data: never, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user = request.session.userId;
+    console.log(user);
+    return user;
   },
 );
