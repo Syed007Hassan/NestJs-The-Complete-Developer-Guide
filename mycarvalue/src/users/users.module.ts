4737d5320040f142eccfa5b88d32d2181441fca1
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-07-09 20:34:40 +0500

    Globally Interceptor added

--- mycarvalue/src/users/users.module.ts
+++ mycarvalue/src/users/users.module.ts
@@ -5,10 +5,20 @@ import { UsersService } from './users.service';
 import { User } from './user.entity';
 import { AuthService } from './auth.service';
 import { CurrentUserInterceptor } from './interceptors/current-user-intetrceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, CurrentUserInterceptor],
+  providers: [
+    UsersService,
+    AuthService,
+    CurrentUserInterceptor,
+    //Globally scoped interceptor
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptor,
+    },
+  ],
 })
 export class UsersModule {}
