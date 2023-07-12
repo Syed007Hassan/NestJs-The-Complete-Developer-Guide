8fb6a1a36f7254481edbe6ad1e2d2d6a21c535ae
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-07-13 01:29:17 +0500

    sign up test added

--- mycarvalue/src/users/auth.service.spec.ts
+++ mycarvalue/src/users/auth.service.spec.ts
@@ -9,8 +9,8 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     const fakeUsersService: Partial<UsersService> = {
-      findByEmail(email) {
-        return Promise.resolve({ id: 1, email, password: 'password' } as User);
+      findByEmail: (email: string) => {
+        return Promise.resolve(undefined);
       },
       create: (email: string, password: string) => {
         return Promise.resolve({ id: 1, email, password } as User);
@@ -32,5 +32,15 @@ describe('AuthService', () => {
 
   it('can create an instance of auth service', async () => {
     expect(service).toBeDefined();
+  });
+
+  it('creates a new user with a salted and hashed password', async () => {
+    const user = await service.signup('sasd@gmail.com', 'password');
+    expect(user.password).not.toEqual('password');
+
+    const [salt, hash] = user.password.split('.');
+
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
   });
 });
