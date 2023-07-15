a5130b9627b0728a6289d72a23129f46c6e5f159
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-07-15 17:01:09 +0500

    unit testing complete

--- mycarvalue/src/users/users.controller.spec.ts
+++ mycarvalue/src/users/users.controller.spec.ts
@@ -3,6 +3,7 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
+import { response } from 'express';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -25,6 +26,12 @@ describe('UsersController', () => {
       },
     };
 
+    fakeAuthService = {
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [
@@ -44,5 +51,35 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findAllU all the users with the given email', async () => {
+    const users = await controller.findAllUsers('hello@gmail.com');
+    expect(users.email).toEqual('hello@gmail.com');
+  });
+
+  it('finds a user by id', async () => {
+    const user = await controller.findUser('1');
+    expect(user.id).toEqual(1);
+  });
+
+  it('findUser throws an error if user not found', async () => {
+    fakeUsersService.findOne = () => null;
+    expect.assertions(1);
+    try {
+      await controller.findUser('1');
+    } catch (err) {
+      expect(err.message).toEqual('user not found');
+    }
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signin(
+      { email: 'a', password: '1' },
+      response as any,
+      session as any,
+    );
+    expect(undefined).toEqual(undefined);
   });
 });
