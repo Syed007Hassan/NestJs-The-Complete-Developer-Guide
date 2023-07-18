44a0686eb367f5a10ee6eb74a00d0c4031d91063
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-07-18 22:48:24 +0500

    admin guard defined

--- mycarvalue/src/guards/admin.guard.ts
+++ mycarvalue/src/guards/admin.guard.ts
@@ -1,3 +1,13 @@
 import { CanActivate, ExecutionContext } from '@nestjs/common';
+import { CurrentUser } from '../users/decorators/current-user-decorator';
 
-async;
+export class AdminGuard implements CanActivate {
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    if (!request.currentUser || !request.currentUser.isAdmin) {
+      return false;
+    }
+
+    return true;
+  }
+}
