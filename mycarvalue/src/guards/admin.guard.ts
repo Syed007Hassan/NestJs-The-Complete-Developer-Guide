3ea84f191f0bbb488248620ea5fe07bcb1235c77
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-07-19 21:45:36 +0500

    guard fixed

--- mycarvalue/src/guards/admin.guard.ts
+++ mycarvalue/src/guards/admin.guard.ts
@@ -4,7 +4,8 @@ import { CurrentUser } from '../users/decorators/current-user-decorator';
 export class AdminGuard implements CanActivate {
   canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
-    if (!request.currentUser || !request.currentUser.isAdmin) {
+
+    if (request.currentUser.admin == undefined || !request.currentUser.admin) {
       return false;
     }
 
--- mycarvalue/src/main.ts
+++ mycarvalue/src/main.ts
@@ -36,7 +36,7 @@ async function bootstrap() {
 
   // app.use(cookieParser());
 
-  // app.use(cookieSession({ keys: ['mysecret'] }));
+  app.use(cookieSession({ keys: ['mysecret'] }));
 
   app.useGlobalPipes(
     new ValidationPipe({
