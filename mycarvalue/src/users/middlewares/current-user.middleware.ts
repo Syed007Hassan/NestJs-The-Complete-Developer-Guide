bc6fffacab688e297f6bdfce40baa951b6abab0f
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-07-19 21:18:53 +0500

    current middleware changed

--- mycarvalue/src/users/middlewares/current-user.middleware.ts
+++ mycarvalue/src/users/middlewares/current-user.middleware.ts
@@ -1,7 +1,16 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UsersService } from '../users.service';
+import { User } from '../user.entity';
 
+// changing the Request interface to include the currentUser property
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
   constructor(private userService: UsersService) {}
@@ -10,7 +19,7 @@ export class CurrentUserMiddleware implements NestMiddleware {
     const { userId } = req.session || {};
     if (userId) {
       const user = await this.userService.findOne(userId);
-      // @ts-ignore
+
       req.currentUser = user;
     }
     next();
