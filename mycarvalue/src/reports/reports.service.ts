485d5273318d7abdfdf11adec221ba8b06db8110
hassan-ali-OTS <hassan.ali@onetechnologyservices.com>
2023-07-20 17:22:24 +0500

    query added for query builder

--- mycarvalue/src/reports/reports.service.ts
+++ mycarvalue/src/reports/reports.service.ts
@@ -4,6 +4,7 @@ import { Report } from './report.entity';
 import { Repository } from 'typeorm';
 import { createReportDTO } from './DTOs/createReportDTO';
 import { User } from 'src/users/user.entity';
+import { GetEstimateDTO } from './DTOs/get-estimateDTO';
 
 @Injectable()
 export class ReportsService {
@@ -26,19 +27,15 @@ export class ReportsService {
     return this.repo.save(report);
   }
 
-  createEstimate({ make, model, lng, lat, year, mileage }: any) {
+  createEstimate(estimateDto: GetEstimateDTO) {
     return this.repo
       .createQueryBuilder()
       .select('AVG(price)', 'price')
-      .where('make = :make', { make })
-      .andWhere('model = :model', { model })
-      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
-      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
-      .andWhere('year - :year BETWEEN -3 AND 3', { year })
-      .andWhere('approved IS TRUE')
-      .orderBy('ABS(mileage - :mileage)', 'DESC')
-      .setParameters({ mileage })
-      .limit(3)
+      .where('make = :make', { make: estimateDto.make })
+      .andWhere('model = :model', { model: estimateDto.model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng: estimateDto.lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat: estimateDto.lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year: estimateDto.year })
       .getRawOne();
   }
 }
